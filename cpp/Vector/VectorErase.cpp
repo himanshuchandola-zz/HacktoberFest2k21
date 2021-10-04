f9d7a867de0240583c20ee83d29da75370e5f788
Vishal S <65108964+blog-a1@users.noreply.github.com>
2021-10-05 02:14:31 +0530

    Added vector erase

---
+++ cpp/Vector/VectorErase.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+
+int main() 
+{
+    vector<int>v;
+    int n,item,a,b,c;
+    cin>>n; 
+    
+    for(int i=0; i<n; i++) 
+    {
+        cin>>item;
+        v.push_back(item);
+    }
+    cin>>a;
+    if(a<v.size()) 
+    { v.erase(v.begin()+a-1);
+    }
+    cin>>b>>c;
+    if(b<c && c<v.size()) 
+    {v.erase(v.begin()+b-1,v.begin()+c-1);
+    }
+    cout<<v.size()<<endl;
+    for(auto vin:v) 
+    {
+        cout<<vin<<" ";
+    }
+    return 0;
+}
